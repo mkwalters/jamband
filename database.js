4f731ab4e4feb06194565b16c4e57baf9f6d26fc
Mitchell <mkwsoftware@gmail.com>
2021-08-03 19:56:31 -0700

    db conn attempt 2

--- database.js
+++ database.js
@@ -1,20 +1,20 @@
 const Pool = require("pg").Pool;
 
-const env = process.env.NODE_ENV || "development";
+// const env = process.env.NODE_ENV || "development";
 
+// let connectionString = {
+//   user: "mitchellwalters",
+//   host: "localhost",
+//   port: 5432,
+//   database: "jamband",
+// };
+
+// if (env !== "development") {
 let connectionString = {
-  user: "mitchellwalters",
-  host: "localhost",
-  port: 5432,
-  database: "jamband",
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
 };
-
-if (env !== "development") {
-  connectionString = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-  };
-}
+// }
 const pool = new Pool(connectionString);
 
 module.exports = pool;
