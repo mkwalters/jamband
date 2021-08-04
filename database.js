9b753333f19bbedb1e6a00f60c883d04bd5cc42a
Mitchell <mkwsoftware@gmail.com>
2021-08-03 20:04:13 -0700

    now try to decide between dev and prod

--- database.js
+++ database.js
@@ -1,20 +1,20 @@
 const Pool = require("pg").Pool;
 
-// const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV || "development";
 
-// let connectionString = {
-//   user: "mitchellwalters",
-//   host: "localhost",
-//   port: 5432,
-//   database: "jamband",
-// };
-
-// if (env !== "development") {
 let connectionString = {
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  user: "mitchellwalters",
+  host: "localhost",
+  port: 5432,
+  database: "jamband",
 };
-// }
+
+if (env !== "development") {
+  let connectionString = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  };
+}
 const pool = new Pool(connectionString);
 
 module.exports = pool;
