4c5ff60f571890520c4ab0126ae6ba6392d61883
Mitchell <mkwsoftware@gmail.com>
2021-08-05 14:08:56 -0700

    give log in and sign up page cards

--- client/src/views/login.jsx
+++ client/src/views/login.jsx
@@ -3,13 +3,26 @@ import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import { Button } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
   root: {
-    display: "block",
+    minWidth: 700,
     margin: "20px",
+    backgroundColor: "#FFFDD0",
+    display: "flex",
   },
-}));
+
+  title: {
+    fontSize: 18,
+  },
+  author: {
+    fontSize: 13,
+  },
+  tree: {},
+});
 
 const Login = () => {
   const classes = useStyles();
@@ -39,7 +52,6 @@ const Login = () => {
   return (
     <main class="container">
       <article class="grid">
-        <h1 className={classes.root}>Log in</h1>
         {/* <div>
           <TextField
             className={classes.root}
@@ -57,46 +69,51 @@ const Login = () => {
             Submit
           </Button>
         </div> */}
-        <div>
-          <hgroup>
-            <h1>Sign in</h1>
-          </hgroup>
-          <form action="/login/password" method="post">
-            <input
-              style={{ display: "block" }}
-              type="text"
-              name="username"
-              placeholder="Username"
-            />
-            <input
-              style={{
-                display: "block",
-                marginTop: "10px",
-                marginBottom: "10px",
-              }}
-              type="password"
-              name="password"
-              placeholder="Password"
-            />
-            <button
-              style={{
-                display: "block",
-                marginTop: "10px",
-                marginBottom: "10px",
-              }}
-              type="submit"
-              class="contrast"
-            >
-              Sign in
-            </button>
-          </form>
-        </div>
-        <div></div>
-        <div>
-          <Link className={classes.root} to="/signup">
-            Create an account
-          </Link>
-        </div>
+
+        <Card className={classes.root}>
+          <CardContent>
+            <div>
+              <hgroup>
+                <h1>Log in</h1>
+              </hgroup>
+              <form action="/login/password" method="post">
+                <input
+                  style={{ display: "block" }}
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                />
+                <input
+                  style={{
+                    display: "block",
+                    marginTop: "10px",
+                    marginBottom: "10px",
+                  }}
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                />
+                <button
+                  style={{
+                    display: "block",
+                    marginTop: "10px",
+                    marginBottom: "10px",
+                  }}
+                  type="submit"
+                  class="contrast"
+                >
+                  Sign in
+                </button>
+              </form>
+            </div>
+            <div>
+              <Link className={classes.root} to="/signup">
+                Create an account
+              </Link>
+            </div>
+          </CardContent>
+          <CardActions></CardActions>
+        </Card>
       </article>
     </main>
   );
--- client/src/views/signup.jsx
+++ client/src/views/signup.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 // import { Link } from "react-router-dom";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles({
+  root: {
+    minWidth: 700,
+    margin: "20px",
+    backgroundColor: "#FFFDD0",
+    display: "flex",
+  },
+
+  title: {
+    fontSize: 18,
+  },
+  author: {
+    fontSize: 13,
+  },
+  tree: {},
+});
 const Signup = () => {
+  const classes = useStyles();
   const style = {
     display: "block",
     marginTop: "10px",
@@ -10,30 +30,33 @@ const Signup = () => {
   return (
     <main class="container">
       <article class="grid">
-        <div>
-          <hgroup>
-            <h1>Sign up</h1>
-          </hgroup>
-          <form action="/users" method="post">
-            <input
-              style={style}
-              type="text"
-              name="username"
-              placeholder="Username"
-            />
-            <input
-              style={style}
-              type="password"
-              name="password"
-              placeholder="Password"
-            />
+        <Card className={classes.root}>
+          <CardContent>
+            <div>
+              <hgroup>
+                <h1>Sign up</h1>
+              </hgroup>
+              <form action="/users" method="post">
+                <input
+                  style={style}
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                />
+                <input
+                  style={style}
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                />
 
-            <button type="submit" class="contrast">
-              Sign up
-            </button>
-          </form>
-        </div>
-        <div></div>
+                <button type="submit" class="contrast">
+                  Sign up
+                </button>
+              </form>
+            </div>
+          </CardContent>
+        </Card>
       </article>
     </main>
   );
