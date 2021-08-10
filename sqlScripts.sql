b5a97f714cc1bab1588d3d961eb2a4c2ae4bfc17
Mitchell <mkwsoftware@gmail.com>
2021-08-10 11:52:55 -0700

    keep track of create table votes

--- sqlScripts.sql
+++ sqlScripts.sql
@@ -6,6 +6,12 @@ CREATE TABLE songs(
 
 
 
+CREATE TABLE votes (
+    song_id INT references songs(song_id),
+    user_id INT references users(user_id),
+    liked BOOL,
+    PRIMARY KEY (song_id, user_id)
+);
 
 
 
@@ -30,49 +36,49 @@ RIGHT JOIN votes ON songs.song_id = votes.song_id);
 
 
 
-SELECT
-    name,
-    (
+  SELECT
+      name,
+      (
+          SELECT
+              COUNT(*)
+          FROM
+              votes
+          WHERE
+              votes.song_id = songs.song_id and votes.liked = true
+      ) -
+      (
+          SELECT
+              COUNT(*)
+          FROM
+              votes
+          WHERE
+              votes.song_id = songs.song_id and votes.liked = false
+      ) as total_votes,
+      (
         SELECT
-            COUNT(*)
+            liked
         FROM
             votes
         WHERE
-            votes.song_id = songs.song_id and votes.liked = true
-    ) -
-    (
-        SELECT
-            COUNT(*)
-        FROM
-            votes
-        WHERE
-            votes.song_id = songs.song_id and votes.liked = false
-    ) as total_votes,
-    (
-      SELECT
-          liked
-      FROM
-          votes
-      WHERE
-          votes.user_id = 12 AND votes.song_id = song.song_id
-    )
-FROM
-    songs,users
-WHERE 
-    songs.author = users.user_id
-ORDER BY
-    (
-        SELECT
-            COUNT(*)
-        FROM
-            votes
-        WHERE
-            votes.song_id = songs.song_id and votes.liked = true
-    ) - (
-        SELECT
-            COUNT(*)
-        FROM
-            votes
-        WHERE
-            votes.song_id = songs.song_id and votes.liked = false
-    ) DESC;
+            votes.user_id = 12 AND votes.song_id = songs.song_id     
+      ) as liked_by_current_user
+  FROM
+      songs,users
+  WHERE 
+      songs.author = users.user_id
+  ORDER BY
+      (
+          SELECT
+              COUNT(*)
+          FROM
+              votes
+          WHERE
+              votes.song_id = songs.song_id and votes.liked = true
+      ) - (
+          SELECT
+              COUNT(*)
+          FROM
+              votes
+          WHERE
+              votes.song_id = songs.song_id and votes.liked = false
+      ) DESC;
