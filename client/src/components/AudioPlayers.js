df665d016f9748e899670405196471cd63623512
Mitchell <mkwsoftware@gmail.com>
2021-08-19 11:32:59 -0700

    remove broken code

--- client/src/components/AudioPlayers.js
+++ client/src/components/AudioPlayers.js
@@ -1,47 +1,11 @@
-import React, { useState, useRef, useEffect } from "react";
+import React from "react";
 import MyAudioPlayer from "./MyAudioPlayer";
-import AudioPlayer from "react-h5-audio-player";
-
-var _ = require("lodash");
 
 const AudioPlayers = (props) => {
-  const [currentSongId, setCurrentSongId] = useState(-1);
-
-  const updateCurrentSongId = (songId) => {
-    setCurrentSongId(songId);
-  };
-
-  const pauseOtherPlayers = (currentId) => {
-    // console.log(audioPlayers.current[0].id);
-
-    console.log(currentId);
-    // if (audioPlayers.current[0].id !== currentId) {
-    //   audioPlayers.current[0].player.audio.current.pause();
-    // }
-
-    // audioPlayers.current.forEach((aPlayer) => {
-    //   if (aPlayer.id !== currentId) {
-    //     if (aPlayer.player) {
-    //       aPlayer.player.audio.current.pause();
-    //     }
-    //   }
-    // });
-  };
   return (
     <div>
-      {props.songData.map((song, index) => (
-        <MyAudioPlayer
-          song={song}
-          pauseOtherPlayers={pauseOtherPlayers}
-          updateCurrentSongId={updateCurrentSongId}
-          id={song.song_id}
-          // ref={(element) =>
-          //   myRefs.current.push({
-          //     id: props.song.song_id,
-          //     // player: element,
-          //   })
-          // }
-        ></MyAudioPlayer>
+      {props.songData.map((song) => (
+        <MyAudioPlayer song={song} id={song.song_id}></MyAudioPlayer>
       ))}
     </div>
   );
